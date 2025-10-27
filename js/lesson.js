473fe437947d2289ef1b6227de19b687eef1d852
trio <ashuubayeva_g@iuca.kg>
2025-10-28 01:21:22 +0600

    tab-slider

--- js/lesson.js
+++ js/lesson.js
@@ -17,7 +17,6 @@ phoneButton.addEventListener('click', ()=>{
 })
 
 
-
 //проверка российского номера
 const phoneInputR = document.querySelector('#phoneR_input');
 const phoneButtonR = document.querySelector('#phoneR_button');
@@ -35,3 +34,54 @@ phoneButtonR.addEventListener('click', () => {
         phoneSpanR.style.color = 'red';
     }
 });
+
+
+const tabsContentCards = document.querySelectorAll('.tab_content_block');
+const tabsItems = document.querySelectorAll('.tab_content_item');
+const tabsItemsParents = document.querySelector('.tab_content_table table'); // исправлено
+
+const hightTabsContentCards = () => {
+    tabsContentCards.forEach((tabsContentCard) => {
+        tabsContentCard.style.display = 'none';
+    });
+    tabsItems.forEach((tabItem) => {
+        tabItem.classList.remove('tab_content_item_active');
+    });
+};
+
+const showTabsContentCards = (indexElement = 0) => {
+    tabsContentCards[indexElement].style.display = 'block';
+    tabsItems[indexElement].classList.add('tab_content_item_active');
+};
+
+hightTabsContentCards();
+showTabsContentCards();
+
+tabsItemsParents.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        clearInterval(intervalId);
+        tabsItems.forEach((tabItem, tabItemIndex) => {
+            if (event.target === tabItem) {
+                hightTabsContentCards();
+                showTabsContentCards(tabItemIndex);
+                curretIndex = tabItemIndex;
+                startAuthoSlider();
+            }
+        });
+    }
+};
+
+let curretIndex = 0; // Первая вкладка
+let intervalId; // Переменная для хранения интервала
+
+// Ф-ция для автоматического переключения
+const startAuthoSlider = () => {
+    intervalId = setInterval(() => {
+        hightTabsContentCards();
+        showTabsContentCards(curretIndex);
+        curretIndex = (curretIndex + 1) % tabsItems.length;
+    }, 2000); // 2сек
+};
+
+// Запуск автослайдера
+startAuthoSlider();
